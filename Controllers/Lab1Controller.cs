bdc5175978fa37fe8ad84c39e3fcddcd57f89121
polyau <polinavigovskaya1@gmail.com>
2023-12-08 02:33:21 +0300

    Добавьте файлы проекта.

---
+++ Controllers/Lab1Controller.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LWI_Vigovskaya.Controllers
+{
+    public class Lab1Controller : Controller
+    {
+        // GET: Lab1
+        public ActionResult FirstViewMethod()
+        {
+            List<string> vegetables = GetVegetablesList();
+            return View(vegetables);
+        }
+
+        public ActionResult SecondViewMethod()
+        {
+            return View(GetVegetablesList().OrderBy(x => x).ToList());
+        }
+
+        public ActionResult ThirdViewMethod()
+        {
+            return View(GetVegetablesList().OrderBy(x => x).ToList());
+        }
+
+
+        public List<string> GetVegetablesList()
+        {
+            List<string> vegetables = new List<string>();
+            vegetables.Add("Томат");
+            vegetables.Add("Огурец");
+            vegetables.Add("Картофель");
+            vegetables.Add("Кабачок");
+            vegetables.Add("Баклажан");
+            vegetables.Add("Капуста");
+            vegetables.Add("Брокколи");
+            vegetables.Add("Перец");
+
+            return vegetables;
+        }
+
+    }
+}
